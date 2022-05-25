ac0ec7746f812b4b57afac3772692802dfb22422
KanHatakeyama <satokan@toki.waseda.jp>
2022-05-25 14:45:32 +0900

    setup

--- setup.py
+++ setup.py
@@ -12,15 +12,21 @@ setup(name='QCLRegressor',
       # packages=find_packages(),
       packages=[
           "QCLRegressor.Encoders",
+          "QCLRegressor.gates",
+          "QCLRegressor.Qiskit",
+          "QCLRegressor.regressors",
+          "QCLRegressor.testing",
+          "QCLRegressor.utils",
       ],
 
-      package_dir={"QCLRegressor.Encoders": "regressions/qcl/Encoders",
-                   # "": "regressions/qcl/gates",
-                   # "": "regressions/qcl/Qiskit",
-                   # "": "regressions/qcl/regressors",
-                   # "": "regressions/qcl/testing",
-                   # "": "regressions/qcl/utils"
-                   },
+      package_dir={
+          "QCLRegressor.Encoders": "regressions/qcl/Encoders",
+          "QCLRegressor.gates": "regressions/qcl/gates",
+          "QCLRegressor.Qiskit": "regressions/qcl/Qiskit",
+          "QCLRegressor.regressors": "regressions/qcl/regressors",
+          "QCLRegressor.testing": "regressions/qcl/testing",
+          "QCLRegressor.utils": "regressions/qcl/utils"
+      },
       )
 """
 packages=['regressions/qcl/Encoders',
